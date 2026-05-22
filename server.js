b5bd4f9e03e902b6cb0cc4bf6cf9b1fc744166fc
nexcoofficial <devakianggis2@gmail.com>
2026-05-22 17:57:20 +0700

    firebase env

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const admin = require("firebase-admin");
 const cors = require("cors");
 
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
