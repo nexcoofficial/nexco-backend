e8ec289dd5dfbc71c258b8b9803d69d055224e32
nexcoofficial <devakianggis2@gmail.com>
2026-05-22 18:03:02 +0700

    use secret file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const admin = require("firebase-admin");
 const cors = require("cors");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = require("/etc/secrets/firebase-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
