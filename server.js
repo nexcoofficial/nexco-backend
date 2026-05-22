607943e9446bbeada91994c71d21440ee225ef31
nexcoofficial <devakianggis2@gmail.com>
2026-05-22 18:17:33 +0700

    home route

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("NEXCO BACKEND RUNNING 🚀");
+});
+
 app.post("/api/login", async (req, res) => {
   try {
     const { email, key, device_id } = req.body;
